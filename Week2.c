4dc375bc527fe8eb54405ed61144d822567a5d73
Kat <karacav@msn.com>
2022-04-03 20:30:13 -0700

    Caesar Cipher Problem draft

--- Week2.c
+++ Week2.c
@@ -63,3 +63,76 @@ int main(void)
     return count_letters(text);
 }
 
+// Number 2 - Caesar Cipher - encrypt a string by moving letters by the number provided by the user
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+bool only_digits(string arg1)
+{
+    for (int i = 0; i < strlen(arg1); i++)
+    {
+        if(isdigit(arg1[i]) == 0) {
+            printf("Usage: ./caesar key\n");
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(int cipherkey, char plainLetter)
+{
+    int normalizeCipherKey = cipherkey;
+// For each character in the plaintext, rotate it using the key
+    if (cipherkey > 26) {
+        normalizeCipherKey = cipherkey % 26;
+        printf("line 26 cipher key 2 %i\n%c\n", normalizeCipherKey, plainLetter);
+      //  printf("line 27 plain text %c\n", plainLetter);
+    }
+    if (isalpha(plainLetter)) {
+    //    if (normalizeCipherKey + )
+        // plainLetter += cipherkey;
+        int finalLetter = atoi(&plainLetter);
+        int finalCipher = finalLetter % 26;
+        finalLetter += finalCipher;
+    }
+    return plainLetter;
+}
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Error: run program with one parameter\n");
+        return 1;
+    }
+    else
+    {
+// make sure all indices in argv are digits
+        if(only_digits(argv[1]))
+        {
+// if they're all digits, convert string to int
+            int cipherkey = atoi(argv[1]);
+       //     printf("line 56 cipher key %i\n", cipherkey);
+
+// Prompt user for plaintext
+            string userText = get_string("plaintext: ");
+
+            printf("ciphertext: ");
+            for (int i = 0; i < strlen(userText); i++)
+            {
+              //  sprintf("%i", "%s", rotate(cipherkey, &userText[i]));
+              char letter = rotate(cipherkey, userText[i]);    // pass int for cipher and char for text
+            printf("line 66 %c", letter);
+            }
+            printf("\n");
+        }
+        else // if user inputs cipher that isn't a digit, return 1
+        {
+            return 1;
+        }
+    }
+}
